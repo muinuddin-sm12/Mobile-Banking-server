139893240eed9be37a79f49d97719039451f5b48
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-08-15 12:35:02 +0400

    add: client site cors origin

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const port = process.env.PORT || 9000;
 
@@ -8,15 +10,17 @@ const app = express();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://mobile-banking-client.vercel.app"
   ],
   credentials: true,
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ff1pkvw.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,8 +29,28 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+// middleware
+const logger = async (req, res, next) => {
+  // console.log('called', req.host, req.originalUrl)
+  next();
+};
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unothorized" });
+    }
+    // console.log(decoded)
+    req.user = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -34,30 +58,49 @@ async function run() {
     // await client.connect();
     const userCollection = client.db("bankingDB").collection("users");
     const requestCollection = client.db("bankingDB").collection("requests");
-    const transactionRequestCollection = client.db("bankingDB").collection("transactionRequests");
+    const transactionRequestCollection = client
+      .db("bankingDB")
+      .collection("transactionRequests");
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
 
     // save an user in database
-    app.post('/users', async(req, res) => {
-        const userData = req.body;
-        const result = await userCollection.insertOne(userData);
-        res.send(result);
-    })
-    app.get('/users', async(req, res) => {
-        const result = await userCollection.find().toArray()
-        res.send(result)
-    })
-    app.get('/users/:id', async(req, res) => {
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+      const result = await userCollection.insertOne(userData);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      console.log(req.query.email);
+      console.log("token owner info", req.user);
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.findOne(query);
       res.send(result);
-    })
+    });
     // update an user in dataBase
     app.put("/users/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id)
+      console.log(id);
       const updateData = req.body;
-      console.log(updateData)
+      console.log(updateData);
       const query = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
@@ -65,49 +108,48 @@ async function run() {
           ...updateData,
         },
       };
-      try{
+      try {
         const result = await userCollection.updateOne(
           query,
           updateDoc,
           options
         );
         res.send(result);
-      }catch(error) {
-        console.log(error)
+      } catch (error) {
+        console.log(error);
       }
     });
-    app.post('/requests', async (req, res) => {
+    app.post("/requests", async (req, res) => {
       const requestData = req.body;
-      console.log(requestData)
-      const result = await requestCollection.insertOne(requestData)
-      res.send(result)
-    })
-    app.get('/requests', async(req, res) =>{
-      const result = await requestCollection.find().toArray()
-      res.send(result)
-    })
+      console.log(requestData);
+      const result = await requestCollection.insertOne(requestData);
+      res.send(result);
+    });
+    app.get("/requests", async (req, res) => {
+      const result = await requestCollection.find().toArray();
+      res.send(result);
+    });
 
-
-    app.post('/transactionRequests', async (req, res) => {
+    app.post("/transactionRequests", async (req, res) => {
       const data = req.body;
-      const result = await transactionRequestCollection.insertOne(data)
-      res.send(result)
-    })
-    app.get('/transactionRequests', async (req, res) => {
-      const result = await transactionRequestCollection.find().toArray()
-      res.send(result)
-    })
-    app.get('/transactionRequests/:id', async(req, res) => {
+      const result = await transactionRequestCollection.insertOne(data);
+      res.send(result);
+    });
+    app.get("/transactionRequests", async (req, res) => {
+      const result = await transactionRequestCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/transactionRequests/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await transactionRequestCollection.findOne(query);
       res.send(result);
-    })
+    });
     app.put("/transactionRequests/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id)
+      console.log(id);
       const updateData = req.body;
-      console.log(updateData)
+      console.log(updateData);
       const query = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
@@ -115,33 +157,30 @@ async function run() {
           ...updateData,
         },
       };
-      try{
+      try {
         const result = await transactionRequestCollection.updateOne(
           query,
           updateDoc,
           options
         );
         res.send(result);
-      }catch(error) {
-        console.log(error)
+      } catch (error) {
+        console.log(error);
       }
     });
 
-
-
-
-
-
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-app.get('/', (req, res) => {
-    res.send('Hello from Mobile Banking Server....')
-}) 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.get("/", (req, res) => {
+  res.send("Hello from Mobile Banking Server....");
+});
+app.listen(port, () => console.log(`Server running on port ${port}`));
